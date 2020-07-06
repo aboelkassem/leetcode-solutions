74967de539978b784eca44956c42c5dc1660a9d7
Mohamed Abdelrahman <42910789+Aboelkassem@users.noreply.github.com>
2020-07-06 14:15:37 +0200

    adding day 6

---
+++ July LeetCoding Challenge/Week 1 July 1st - July 7th/Plus One.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+}
