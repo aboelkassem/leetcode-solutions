1ab1bc33d01e954c7f50a761ff41df36c1cc0406
Mohamed <aboelkassem.me@gmail.com>
2023-08-14 20:37:42 +0300

    feat: add another solution for Linked List Cycle

--- grind75/week 1/11. Linked List Cycle.cs
+++ grind75/week 1/11. Linked List Cycle.cs
@@ -29,3 +29,24 @@ public class Solution {
         return false;
     }
 }
+
+
+// ======= using Two Pointers
+// Time: O(n)
+// Space: O(1)
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;  
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
