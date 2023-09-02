0b2a9b60a7a04cdef4a55e9f2763c88de4a162c1
Mohamed <aboelkassem.me@gmail.com>
2023-09-02 18:27:30 +0300

    feat: add middle-of-the-linked-list

---
+++ grind75/week 1/21. Middle of the Linked List.cs
@@ -0,0 +1,29 @@
+// Link: https://leetcode.com/problems/middle-of-the-linked-list
+
+// Time: O(n)
+// Space: O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
