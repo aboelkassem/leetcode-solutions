06ea3b5c01c379dd12e89d80e6cd68a1e3b95cc0
Restyled.io <commits@restyled.io>
2023-10-06 10:30:32 +0000

    Restyled by clang-format

--- 27.Number of Provinces.cs
+++ 27.Number of Provinces.cs
@@ -5,34 +5,30 @@
 // Space: O(n)
 
 public class Solution {
-    public int FindCircleNum(int[][] isConnected) {
-        var vis = new bool[isConnected.Length];
+  public int FindCircleNum(int[][] isConnected) {
+    var vis = new bool[isConnected.Length];
 
-        var counter = 0;
-        for (int i = 0; i < isConnected.Length; i++)
-        {
-            // Check Connected Component
-            if (!vis[i])
-            {
-                counter++;
-                Traverse(i, ref isConnected, ref vis);
-            }
-        }
-
-        return counter;
+    var counter = 0;
+    for (int i = 0; i < isConnected.Length; i++) {
+      // Check Connected Component
+      if (!vis[i]) {
+        counter++;
+        Traverse(i, ref isConnected, ref vis);
+      }
     }
 
-    // Traverse Graph and consider one province (connected component)
-    public static void Traverse(int cur, ref int[][] isConnected, ref bool[] vis)
-    {
-        // DFS Traverse
-        vis[cur] = true;
-        for (int next = 0; next < isConnected.Length; next++)
-        {
-            if (isConnected[cur][next] == 1 && !vis[next])
-            {
-                Traverse(next, ref isConnected, ref vis);
-            }
-        }
+    return counter;
+  }
+
+  // Traverse Graph and consider one province (connected component)
+  public static void Traverse(int cur, ref int[][] isConnected,
+                              ref bool[] vis) {
+    // DFS Traverse
+    vis[cur] = true;
+    for (int next = 0; next < isConnected.Length; next++) {
+      if (isConnected[cur][next] == 1 && !vis[next]) {
+        Traverse(next, ref isConnected, ref vis);
+      }
     }
+  }
 }
