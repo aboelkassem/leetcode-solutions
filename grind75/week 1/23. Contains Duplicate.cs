fbd755c5d84ce6c66b6a14376f543617f35c7ba5
Restyled.io <commits@restyled.io>
2023-10-06 10:28:48 +0000

    Restyled by clang-format

--- grind75/week 1/23. Contains Duplicate.cs
+++ grind75/week 1/23. Contains Duplicate.cs
@@ -4,14 +4,14 @@
 // Space: O(n)
 
 public class Solution {
-    public bool ContainsDuplicate(int[] nums) {
-        var set = new HashSet<int>();
-        foreach (var num in nums) {
-            if (set.Contains(num))
-                return true;
+  public bool ContainsDuplicate(int[] nums) {
+    var set = new HashSet<int>();
+    foreach (var num in nums) {
+      if (set.Contains(num))
+        return true;
 
-            set.Add(num);
-        }
-        return false;
+      set.Add(num);
     }
+    return false;
+  }
 }
