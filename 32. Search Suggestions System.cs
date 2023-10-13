7bfd9ab399639cddbc492b34e846afe1b77b12ae
Restyled.io <commits@restyled.io>
2023-10-13 06:41:11 +0000

    Restyled by clang-format

--- 32. Search Suggestions System.cs
+++ 32. Search Suggestions System.cs
@@ -1,108 +1,92 @@
 // Link: https://leetcode.com/problems/search-suggestions-system
 
-
 // Time: O(n) where n is the length of the word
-// Space: O(n) where n is the length of the word (in worst case if empty and will add all word characters)
+// Space: O(n) where n is the length of the word (in worst case if empty and
+// will add all word characters)
 
 public class Solution {
-    public IList<IList<string>> SuggestedProducts(string[] products, string searchWord) {
-        var trie = new Trie();
-        var result = new List<IList<string>>();
-        foreach (var product in products)
-        {
-            trie.Insert(product);
-        }
-        string prefix = string.Empty;
-        foreach (var c in searchWord)
-        {
-            prefix += c;
-            result.Add(trie.Suggest(prefix));
-        }
-
-        return result;
+  public IList<IList<string>> SuggestedProducts(string[] products,
+                                                string searchWord) {
+    var trie = new Trie();
+    var result = new List<IList<string>>();
+    foreach (var product in products) {
+      trie.Insert(product);
     }
+    string prefix = string.Empty;
+    foreach (var c in searchWord) {
+      prefix += c;
+      result.Add(trie.Suggest(prefix));
+    }
+
+    return result;
+  }
 }
 
-
-public class TrieNode
-{
-    public bool isWord;
-    public Dictionary<char, TrieNode> children;
-    public TrieNode()
-    {
-        this.children = new();
-    }
+public class TrieNode {
+  public bool isWord;
+  public Dictionary<char, TrieNode> children;
+  public TrieNode() { this.children = new(); }
 }
 
-public class Trie
-{
-    private TrieNode root;
-    public Trie()
-    {
-        this.root = new TrieNode();
+public class Trie {
+  private TrieNode root;
+  public Trie() { this.root = new TrieNode(); }
+
+  public void Insert(string word) {
+    TrieNode cur = this.root;
+
+    for (int i = 0; i < word.Length; i++) {
+      char c = word[i];
+
+      if (!cur.children.ContainsKey(c))
+        cur.children.Add(c, new TrieNode());
+
+      cur = cur.children[c]; // continue traversal
     }
 
-    public void Insert(string word)
-    {
-        TrieNode cur = this.root;
+    cur.isWord = true;
+  }
 
-        for (int i = 0; i < word.Length; i++)
-        {
-            char c = word[i];
+  public List<string> Suggest(string prefix) {
+    var result = new List<string>();
+    TrieNode cur = this.root;
 
-            if (!cur.children.ContainsKey(c))
-                cur.children.Add(c, new TrieNode());
+    for (int i = 0; i < prefix.Length; i++) {
+      char c = prefix[i];
 
-            cur = cur.children[c]; // continue traversal
-        }
+      if (!cur.children.ContainsKey(c))
+        return result; // empty result
 
-        cur.isWord = true;
+      cur = cur.children[c]; // continue traversal
     }
 
-    public List<string> Suggest(string prefix)
-    {
-        var result = new List<string>();
-        TrieNode cur = this.root;
+    // return top 3 suggestions
+    SuggestTopThree(cur, prefix, ref result);
+    return result;
+  }
 
-        for (int i = 0; i < prefix.Length; i++)
-        {
-            char c = prefix[i];
+  private void SuggestTopThree(TrieNode cur, string prefix,
+                               ref List<string> result) {
+    // Using DFS
 
-            if (!cur.children.ContainsKey(c))
-                return result; // empty result
+    // Base case 1: reached max suggestions
+    if (result.Count == 3)
+      return;
 
-            cur = cur.children[c]; // continue traversal
-        }
+    // Base case 2: is word
+    if (cur.isWord)
+      result.Add(prefix);
 
-        // return top 3 suggestions
-        SuggestTopThree(cur, prefix, ref result);
-        return result;
+    // Run all possible paths
+    for (char c = 'a'; c <= 'z'; c++) {
+      if (cur.children.ContainsKey(c)) {
+        prefix += c;
+        SuggestTopThree(cur.children[c], prefix, ref result);
+        // backtrack // remove last character // latest added character
+        // we just try with each possible character
+        // because we didn't want to add it to prefix permanently
+        prefix = prefix.Remove(prefix.Length - 1);
+      }
     }
-
-    private void SuggestTopThree(TrieNode cur, string prefix, ref List<string> result)
-    {
-        // Using DFS
-
-        // Base case 1: reached max suggestions
-        if (result.Count == 3)
-            return;
-
-        // Base case 2: is word
-        if (cur.isWord)
-            result.Add(prefix);
-
-        // Run all possible paths
-        for (char c = 'a'; c <= 'z'; c++)
-        {
-            if (cur.children.ContainsKey(c))
-            {
-                prefix += c;
-                SuggestTopThree(cur.children[c], prefix, ref result);
-                // backtrack // remove last character // latest added character
-                // we just try with each possible character
-                // because we didn't want to add it to prefix permanently
-                prefix = prefix.Remove(prefix.Length - 1);
-            }
-        }
-    }
+  }
 }
