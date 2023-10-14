08213e084b1744649c6e6d33b4a04d8a4a6a618e
Restyled.io <commits@restyled.io>
2023-10-14 15:25:50 +0000

    Restyled by astyle

--- 38.Uncrossed Lines.cs
+++ 38.Uncrossed Lines.cs
@@ -19,21 +19,21 @@ public class Solution {
         // base case
         if (idx1 >= nums1.Length || idx2 >= nums2.Length)
             return 0;
-        
-				// memoization
-				if (dp[idx1, idx2] != -1)
-				    return dp[idx1, idx2];
-				
-				// transition
-				if (nums1[idx1] == nums2[idx2])
-				    // if the numbers are equal, then we can draw a line
-				    dp[idx1, idx2] = 1 + Recurse(idx1 + 1, idx2 + 1, nums1, nums2, dp);
-				else
-				    // if the numbers are not equal, then we can't draw a line
-				    // so we move to the next number in nums1 or nums2
-				    // and take the max of the two
-				    // this is because we want to maximize the number of lines we can draw
-				    dp[idx1, idx2] = Math.Max(Recurse(idx1 + 1, idx2, nums1, nums2, dp), Recurse(idx1, idx2 + 1, nums1, nums2, dp));
+
+        // memoization
+        if (dp[idx1, idx2] != -1)
+            return dp[idx1, idx2];
+
+        // transition
+        if (nums1[idx1] == nums2[idx2])
+            // if the numbers are equal, then we can draw a line
+            dp[idx1, idx2] = 1 + Recurse(idx1 + 1, idx2 + 1, nums1, nums2, dp);
+        else
+            // if the numbers are not equal, then we can't draw a line
+            // so we move to the next number in nums1 or nums2
+            // and take the max of the two
+            // this is because we want to maximize the number of lines we can draw
+            dp[idx1, idx2] = Math.Max(Recurse(idx1 + 1, idx2, nums1, nums2, dp), Recurse(idx1, idx2 + 1, nums1, nums2, dp));
         return dp[idx1, idx2];
     }
 }
