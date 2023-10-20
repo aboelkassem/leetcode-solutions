eb38eadb12f6e90882485906da079482da05bc30
Restyled.io <commits@restyled.io>
2023-10-20 08:45:27 +0000

    Restyled by clang-format

--- 39. Backspace String Compare .cs
+++ 39. Backspace String Compare .cs
@@ -5,21 +5,19 @@
 // Space: O(n)
 
 public class Solution {
-    public bool BackspaceCompare(string s, string t) {
-        return CleanString(s) == CleanString(t);
+  public bool BackspaceCompare(string s, string t) {
+    return CleanString(s) == CleanString(t);
+  }
+
+  private static string CleanString(string s) {
+    Stack<char> stack = new Stack<char>();
+    foreach (var c in s) {
+      if (c != '#')
+        stack.Push(c);
+      else if (stack.Count > 0) // remove latest added one
+        stack.Pop();
     }
 
-    private static string CleanString(string s)
-    {
-        Stack<char> stack = new Stack<char>();
-        foreach (var c in s)
-        {
-            if (c != '#')
-                stack.Push(c);
-            else if (stack.Count > 0) // remove latest added one
-                stack.Pop();
-        }
-
-        return String.Join(",", stack);
-    }
+    return String.Join(",", stack);
+  }
 }
