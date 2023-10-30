067beb55dc34e76911830b2ca01f30feb8868f89
Restyled.io <commits@restyled.io>
2023-10-30 10:03:16 +0000

    Restyled by astyle

--- 40.Intersection of Two Linked Lists.cs
+++ 40.Intersection of Two Linked Lists.cs
@@ -4,43 +4,43 @@
 // Time: O(n)
 // Space: O(1)
 public static ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-  var a = headA;
-  var b = headB;
-  while (a != b) {
-    a = a == null ? headB : a.next;
-    b = b == null ? headA : b.next;
-  }
-  return a;
+    var a = headA;
+    var b = headB;
+    while (a != b) {
+        a = a == null ? headB : a.next;
+        b = b == null ? headA : b.next;
+    }
+    return a;
 }
 
 // using stack (some cases not valid because pointer)
 // Time: O(n)
 // Space: O(n)
 public static ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-  if (headA == null || headB == null)
-    return null;
+    if (headA == null || headB == null)
+        return null;
 
-  var stackA = new Stack<ListNode>();
-  var stackB = new Stack<ListNode>();
+    var stackA = new Stack<ListNode>();
+    var stackB = new Stack<ListNode>();
 
-  ListNode result = null;
+    ListNode result = null;
 
-  while (headA != null) {
-    stackA.Push(headA);
-    headA = headA.next;
-  }
+    while (headA != null) {
+        stackA.Push(headA);
+        headA = headA.next;
+    }
 
-  while (headB != null) {
-    stackB.Push(headB);
-    headB = headB.next;
-  }
+    while (headB != null) {
+        stackB.Push(headB);
+        headB = headB.next;
+    }
 
-  while (stackA.Count > 0 && stackB.Count > 0 &&
-         stackA.Peek().val == stackB.Peek().val) {
-    result = stackA.Peek();
-    stackA.Pop();
-    stackB.Pop();
-  }
+    while (stackA.Count > 0 && stackB.Count > 0 &&
+            stackA.Peek().val == stackB.Peek().val) {
+        result = stackA.Peek();
+        stackA.Pop();
+        stackB.Pop();
+    }
 
-  return result;
+    return result;
 }
