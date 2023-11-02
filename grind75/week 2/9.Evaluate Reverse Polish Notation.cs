859abd6abb57327c3c88e5ecbe6e6f52651571e6
Restyled.io <commits@restyled.io>
2023-11-02 15:23:20 +0000

    Restyled by clang-format

--- grind75/week 2/9.Evaluate Reverse Polish Notation.cs
+++ grind75/week 2/9.Evaluate Reverse Polish Notation.cs
@@ -5,55 +5,45 @@
 // Space: O(n)
 
 public class Solution {
-    public int EvalRPN(string[] tokens) {
-        // special case
-        if (tokens.Length == 1)
-            return Convert.ToInt32(tokens[0]);
+  public int EvalRPN(string[] tokens) {
+    // special case
+    if (tokens.Length == 1)
+      return Convert.ToInt32(tokens[0]);
 
-        var result = 0;
+    var result = 0;
 
-        var stack = new Stack<int>();
+    var stack = new Stack<int>();
 
-        foreach (var token in tokens)
-        {
-            if (token == "/")
-            {
-                stack.TryPop(out var firstElement);
-                stack.TryPop(out var secondElement);
+    foreach (var token in tokens) {
+      if (token == "/") {
+        stack.TryPop(out var firstElement);
+        stack.TryPop(out var secondElement);
 
-                result = secondElement / firstElement;
-                stack.Push(result);
-            }
-            else if (token == "*")
-            {
-                stack.TryPop(out var firstElement);
-                stack.TryPop(out var secondElement);
+        result = secondElement / firstElement;
+        stack.Push(result);
+      } else if (token == "*") {
+        stack.TryPop(out var firstElement);
+        stack.TryPop(out var secondElement);
 
-                result = secondElement * firstElement;
-                stack.Push(result);
-            }
-            else if (token == "+")
-            {
-                stack.TryPop(out var firstElement);
-                stack.TryPop(out var secondElement);
+        result = secondElement * firstElement;
+        stack.Push(result);
+      } else if (token == "+") {
+        stack.TryPop(out var firstElement);
+        stack.TryPop(out var secondElement);
 
-                result = secondElement + firstElement;
-                stack.Push(result);
-            }
-            else if (token == "-")
-            {
-                stack.TryPop(out var firstElement);
-                stack.TryPop(out var secondElement);
+        result = secondElement + firstElement;
+        stack.Push(result);
+      } else if (token == "-") {
+        stack.TryPop(out var firstElement);
+        stack.TryPop(out var secondElement);
 
-                result = secondElement - firstElement;
-                stack.Push(result);
-            }
-            else
-            {
-                stack.Push(Convert.ToInt32(token));
-            }
-        }
-
-        return result;
+        result = secondElement - firstElement;
+        stack.Push(result);
+      } else {
+        stack.Push(Convert.ToInt32(token));
+      }
     }
+
+    return result;
+  }
 }
