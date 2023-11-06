0ca1a70190fc2f6e28596b51eaaa8dc19a9d85fe
Restyled.io <commits@restyled.io>
2023-11-06 13:20:51 +0000

    Restyled by clang-format

--- 44.Linked List Cycle II.cs
+++ 44.Linked List Cycle II.cs
@@ -15,57 +15,52 @@
  * }
  */
 public class Solution {
-    public ListNode DetectCycle(ListNode head) {
-        if (head == null || head.next == null)
-            return null;
+  public ListNode DetectCycle(ListNode head) {
+    if (head == null || head.next == null)
+      return null;
 
-        var slow = head;
-        var fast = head;
+    var slow = head;
+    var fast = head;
 
-        while (fast != null && fast.next != null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
 
-            if (slow == fast)
-                break;
-        }
-
-        // No cycle
-        if (fast == null || fast.next == null)
-            return null;
-
-        slow = head;
-
-        // Find the start of the cycle
-
-        while (slow != fast)
-        {
-            slow = slow.next;
-            fast = fast.next;
-        }
-
-        return slow;
+      if (slow == fast)
+        break;
     }
-}
 
+    // No cycle
+    if (fast == null || fast.next == null)
+      return null;
+
+    slow = head;
+
+    // Find the start of the cycle
+
+    while (slow != fast) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow;
+  }
+}
 
 // using hashset
 // Time: O(n)
 // Space: O(n)
-public class Solution
-{
-    public ListNode DetectCycle(ListNode head)
-    {
-        HashSet<ListNode> visited = new();
+public class Solution {
+  public ListNode DetectCycle(ListNode head) {
+    HashSet<ListNode> visited = new();
 
-        while (head is not null)
-        {
-            if (visited.Contains(head)) return head;
-            visited.Add(head);
-            head = head.next;
-        }
-
-        return null;
+    while (head is not null) {
+      if (visited.Contains(head))
+        return head;
+      visited.Add(head);
+      head = head.next;
     }
+
+    return null;
+  }
 }
