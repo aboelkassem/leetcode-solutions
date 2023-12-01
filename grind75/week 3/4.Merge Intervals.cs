bc4ee110e32fe4252e58ed51977c14cf1305fae6
Restyled.io <commits@restyled.io>
2023-12-01 11:28:17 +0000

    Restyled by clang-format

--- grind75/week 3/4.Merge Intervals.cs
+++ grind75/week 3/4.Merge Intervals.cs
@@ -4,28 +4,24 @@
 // Time: O(nlogn) for sorting
 // Space: O(n)
 public class Solution {
-    public int[][] Merge(int[][] intervals) {
-        var result = new List<int[]>();
-        Array.Sort(intervals, (arr1, arr2) => arr1[0].CompareTo(arr2[0]));
-        var currentInterval = intervals[0];
+  public int[][] Merge(int[][] intervals) {
+    var result = new List<int[]>();
+    Array.Sort(intervals, (arr1, arr2) => arr1[0].CompareTo(arr2[0]));
+    var currentInterval = intervals[0];
+    result.Add(currentInterval);
+    foreach (var interval in intervals) {
+      var currentEnd = currentInterval[1];
+      var nextBegin = interval[0];
+      var nextEnd = interval[1];
+      if (currentEnd >= nextBegin) {
+        // overlap
+        // merge by extending the end of the current interval
+        currentInterval[1] = Math.Max(currentEnd, nextEnd);
+      } else {
+        currentInterval = interval;
         result.Add(currentInterval);
-        foreach (var interval in intervals)
-        {
-            var currentEnd = currentInterval[1];
-            var nextBegin = interval[0];
-            var nextEnd = interval[1];
-            if (currentEnd >= nextBegin)
-            {
-                // overlap
-                // merge by extending the end of the current interval
-                currentInterval[1] = Math.Max(currentEnd, nextEnd);
-            }
-            else
-            {
-                currentInterval = interval;
-                result.Add(currentInterval);
-            }
-        }
-        return result.ToArray();
+      }
     }
+    return result.ToArray();
+  }
 }
