3b761199b7c076041e020862e453c99ec98f20e6
Restyled.io <commits@restyled.io>
2023-12-01 07:58:21 +0000

    Restyled by clang-format

--- grind75/week 3/1.Search in Rotated Sorted Array.cs
+++ grind75/week 3/1.Search in Rotated Sorted Array.cs
@@ -5,28 +5,28 @@
 // Space: O(1)
 
 public int Search(int[] nums, int target) {
-    int left = 0, right = nums.Length - 1;
-    while (left <= right) {
-        // int mid = (left + right) / 2;
-        var mid = left + (right - left) / 2;
+  int left = 0, right = nums.Length - 1;
+  while (left <= right) {
+    // int mid = (left + right) / 2;
+    var mid = left + (right - left) / 2;
 
-        if (nums[mid] == target)
-            return mid;
+    if (nums[mid] == target)
+      return mid;
 
-        if (nums[left] <= nums[mid]) {
-            // range left and mid
-            if (target > nums[mid] || target < nums[left])
-                left = mid + 1;
-            else
-                right = mid - 1;
-        } else {
-            // range right and mid
-            if (target < nums[mid] && target > nums[right])
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
+    if (nums[left] <= nums[mid]) {
+      // range left and mid
+      if (target > nums[mid] || target < nums[left])
+        left = mid + 1;
+      else
+        right = mid - 1;
+    } else {
+      // range right and mid
+      if (target < nums[mid] && target > nums[right])
+        right = mid - 1;
+      else
+        left = mid + 1;
     }
+  }
 
-    return -1;
+  return -1;
 }
