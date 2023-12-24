dc480a624218ca84bdda38c1c432cd61b4387a9d
Restyled.io <commits@restyled.io>
2023-12-24 20:44:17 +0000

    Restyled by astyle

--- grind75/week 3/6.Time Based Key-Value Store.cs
+++ grind75/week 3/6.Time Based Key-Value Store.cs
@@ -6,31 +6,31 @@
 public class TimeMap {
     Dictionary<string,List<(int,string)>> map = new Dictionary<string,List<(int,string)>>();
     public TimeMap() {
-        
+
     }
-    
+
     public void Set(string key, string value, int timestamp) {
-        if(!map.ContainsKey(key)){
+        if(!map.ContainsKey(key)) {
             map[key] = new List<(int, string)>();
         }
         map[key].Add((timestamp,value));
     }
-    
+
     public string Get(string key, int timestamp) {
-        if(!map.ContainsKey(key)){
+        if(!map.ContainsKey(key)) {
             return "";
         }
         var list = map[key];
         int left=0,right=list.Count()-1;
-        while(left<=right){
+        while(left<=right) {
             int mid = (left+right)/2;
-            if(list[mid].Item1==timestamp){
+            if(list[mid].Item1==timestamp) {
                 return list[mid].Item2;
             }
-            else if(list[mid].Item1 < timestamp){
+            else if(list[mid].Item1 < timestamp) {
                 left = mid+1;
             }
-            else{
+            else {
                 right=mid-1;
             }
         }
