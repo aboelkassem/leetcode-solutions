c4b338f301aa40b04049bc2dc9c8d6569e040a68
Restyled.io <commits@restyled.io>
2023-12-29 09:17:05 +0000

    Restyled by clang-format

--- grind75/week 3/8.Sort Colors.cs
+++ grind75/week 3/8.Sort Colors.cs
@@ -3,57 +3,50 @@
 // Using Two pass Count
 // Time: O(n)
 // Space: O(1)
-public void SortColors(int[] nums)
-{
-    int[] count = new int[3];
+public void SortColors(int[] nums) {
+  int[] count = new int[3];
 
-    // Count each color
-    foreach (int num in nums)
-    {
-        count[num]++;
+  // Count each color
+  foreach (int num in nums) {
+    count[num]++;
+  }
+
+  int writeInd = 0;
+
+  // Fill in the values
+  for (int i = 0; i < 3; i++) {
+    while (count[i] > 0) {
+      nums[writeInd] = i;
+      count[i]--;
+      writeInd++;
     }
-
-    int writeInd = 0;
-
-    // Fill in the values
-    for (int i = 0; i < 3; i++)
-    {
-        while (count[i] > 0)
-        {
-            nums[writeInd] = i;
-            count[i]--;
-            writeInd++;
-        }
-    }
+  }
 }
 
 // Using Two pointers (One-pass)
 // Time: O(n)
 // Space: O(1)
-public void SortColors(int[] nums)
-{
-    int zero = 0;
-    int one = 0;
-    int two = nums.Length - 1;
+public void SortColors(int[] nums) {
+  int zero = 0;
+  int one = 0;
+  int two = nums.Length - 1;
 
-    while (one <= two)
-    {
-        switch (nums[one])
-        {
-        case 0:
-            (nums[zero], nums[one]) = (nums[one], nums[zero]); // Swap
-            zero++;
-            one++;
-            break;
+  while (one <= two) {
+    switch (nums[one]) {
+    case 0:
+      (nums[zero], nums[one]) = (nums[one], nums[zero]); // Swap
+      zero++;
+      one++;
+      break;
 
-        case 1:
-            one++;
-            break;
+    case 1:
+      one++;
+      break;
 
-        case 2:
-            (nums[one], nums[two]) = (nums[two], nums[one]); // Swap
-            two--;
-            break;
-        }
+    case 2:
+      (nums[one], nums[two]) = (nums[two], nums[one]); // Swap
+      two--;
+      break;
     }
+  }
 }
