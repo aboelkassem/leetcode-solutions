2c380edd1084eb680e64d288e88cbb27ff95165f
Restyled.io <commits@restyled.io>
2024-01-05 09:11:53 +0000

    Restyled by clang-format

--- 46.Longest Increasing Subsequence.cs
+++ 46.Longest Increasing Subsequence.cs
@@ -3,17 +3,15 @@
 // Time: O(n^2)
 // Space: O(n)
 public class Solution {
-    public int LengthOfLIS(int[] nums) {
-        var lis = new int[nums.Length];
-        for (int i = nums.Length - 1; i >= 0; i--)
-        {
-            lis[i] = 1;
-            for (int j = i + 1; j < nums.Length; j++)
-            {
-                if (nums[i] < nums[j])
-                    lis[i] = Math.Max(lis[i], 1 + lis[j]);
-            }
-        }
-        return lis.Max();
+  public int LengthOfLIS(int[] nums) {
+    var lis = new int[nums.Length];
+    for (int i = nums.Length - 1; i >= 0; i--) {
+      lis[i] = 1;
+      for (int j = i + 1; j < nums.Length; j++) {
+        if (nums[i] < nums[j])
+          lis[i] = Math.Max(lis[i], 1 + lis[j]);
+      }
     }
+    return lis.Max();
+  }
 }
