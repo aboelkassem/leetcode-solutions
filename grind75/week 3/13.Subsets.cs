f52c94e5908d066b371e97e91b7b918b410e01d6
Restyled.io <commits@restyled.io>
2024-01-18 07:24:15 +0000

    Restyled by clang-format

--- grind75/week 3/13.Subsets.cs
+++ grind75/week 3/13.Subsets.cs
@@ -4,49 +4,45 @@
 // Time:  O(2^n)
 // Space:  O(n)
 public class Solution {
-    public IList<IList<int>> Subsets(int[] nums) {
-        IList<IList<int>> subsets = new List<IList<int>>
-        {
-            new List<int>()
-        };
+  public IList<IList<int>> Subsets(int[] nums) {
+    IList<IList<int>> subsets = new List<IList<int>> { new List<int>() };
 
-        for (int i = 0; i < nums.Length; i++)
-        {
-            // previous subsets
-            int count = subsets.Count;
-            for (int j = 0; j < count; j++)
-            {
-                // create a new subset from the existing subset and add the current element to it
-                var temp = subsets[j].ToList();
-                temp.Add(nums[i]);
-                subsets.Add(temp);
-            }
-        }
-        return subsets;
+    for (int i = 0; i < nums.Length; i++) {
+      // previous subsets
+      int count = subsets.Count;
+      for (int j = 0; j < count; j++) {
+        // create a new subset from the existing subset and add the current
+        // element to it
+        var temp = subsets[j].ToList();
+        temp.Add(nums[i]);
+        subsets.Add(temp);
+      }
     }
+    return subsets;
+  }
 }
 
 // Using backtracking
-// Time:  O(2^n) since you explore all possible subsets, and for each subset, you perform constant time operations to add or remove elements from the subset list.
-// Space:  O(n)
+// Time:  O(2^n) since you explore all possible subsets, and for each subset,
+// you perform constant time operations to add or remove elements from the
+// subset list. Space:  O(n)
 public class Solution {
-    public IList<IList<int>> Subsets(int[] nums) {
-        var result = new List<IList<int>>();
+  public IList<IList<int>> Subsets(int[] nums) {
+    var result = new List<IList<int>>();
 
-        Search(nums, 0, new List<int>(10), result);
+    Search(nums, 0, new List<int>(10), result);
 
-        return result;
+    return result;
+  }
+
+  private void Search(int[] nums, int index, IList<int> temp,
+                      IList<IList<int>> result) {
+    result.Add(temp.ToArray());
+
+    for (int i = index; i < nums.Length; i++) {
+      temp.Add(nums[i]);
+      Search(nums, i + 1, temp, result);
+      temp.RemoveAt(temp.Count - 1);
     }
-
-    private void Search(int[] nums, int index, IList<int> temp, IList<IList<int>> result)
-    {
-        result.Add(temp.ToArray());
-
-        for (int i = index; i < nums.Length; i++)
-        {
-            temp.Add(nums[i]);
-            Search(nums, i + 1, temp, result);
-            temp.RemoveAt(temp.Count - 1);
-        }
-    }
+  }
 }
