5cec5d2daeefee96e8bad465cf69807695e8889b
Restyled.io <commits@restyled.io>
2024-01-19 11:35:03 +0000

    Restyled by clang-format

--- grind75/week 3/15.Longest Palindromic Substring.cs
+++ grind75/week 3/15.Longest Palindromic Substring.cs
@@ -4,91 +4,81 @@
 // Time:  O(n^3)
 // Space:  O(1)
 public class Solution {
-    public string LongestPalindrome(string s) {
-        for (int length = s.Length; length > 0; length--)
-        {
-            for (int start = 0; start <= s.Length - length; start++)
-            {
-                if (IsPalindrome(start, start + length, s))
-                {
-                    return s.Substring(start, length);
-                }
-            }
+  public string LongestPalindrome(string s) {
+    for (int length = s.Length; length > 0; length--) {
+      for (int start = 0; start <= s.Length - length; start++) {
+        if (IsPalindrome(start, start + length, s)) {
+          return s.Substring(start, length);
         }
-
-        return "";
+      }
     }
 
-    public bool IsPalindrome(int i, int j, string s)
-    {
-        // using two pointers
-        int left = i;
-        int right = j - 1;
+    return "";
+  }
 
-        while (left < right)
-        {
-            if (s[left] != s[right])
-                return false;
+  public bool IsPalindrome(int i, int j, string s) {
+    // using two pointers
+    int left = i;
+    int right = j - 1;
 
-            left++;
-            right--;
-        }
-        return true;
+    while (left < right) {
+      if (s[left] != s[right])
+        return false;
+
+      left++;
+      right--;
     }
+    return true;
+  }
 }
 
 // Using expand from center and two pointers
 // Time:  O(n^2)
 // Space:  O(1)
 public class Solution {
-    public string LongestPalindrome(string s) {
-        if (s.Length < 2)
-            return s;
+  public string LongestPalindrome(string s) {
+    if (s.Length < 2)
+      return s;
 
-        var res = string.Empty;
-        var resLen = 0;
+    var res = string.Empty;
+    var resLen = 0;
 
-        for (int i = 0; i < s.Length; i++)
-        {
-            // Case 1:
-            // odd length
-            // initial two pointers at the center
-            var left = i;
-            var right = i;
+    for (int i = 0; i < s.Length; i++) {
+      // Case 1:
+      // odd length
+      // initial two pointers at the center
+      var left = i;
+      var right = i;
 
-            // expand outwards
-            // if this condition satisfies, then its a palindrome
-            while (left >=0 && right < s.Length && s[left] == s[right])
-            {
-                var palindromeLength = right - left + 1;
-                if (palindromeLength > resLen)
-                {
-                    res = s.Substring(left, palindromeLength);
-                    resLen = palindromeLength;
-                }
-
-                left--;
-                right++;
-            }
-
-            // Case 2: even length
-            // even length
-            left = i;
-            right = i + 1;
-            while (left >= 0 && right < s.Length && s[left] == s[right])
-            {
-                var palindromeLength = right - left + 1;
-                if (palindromeLength > resLen)
-                {
-                    res = s.Substring(left, palindromeLength);
-                    resLen = palindromeLength;
-                }
-
-                left--;
-                right++;
-            }
+      // expand outwards
+      // if this condition satisfies, then its a palindrome
+      while (left >= 0 && right < s.Length && s[left] == s[right]) {
+        var palindromeLength = right - left + 1;
+        if (palindromeLength > resLen) {
+          res = s.Substring(left, palindromeLength);
+          resLen = palindromeLength;
         }
 
-        return res;
+        left--;
+        right++;
+      }
+
+      // Case 2: even length
+      // even length
+      left = i;
+      right = i + 1;
+      while (left >= 0 && right < s.Length && s[left] == s[right]) {
+        var palindromeLength = right - left + 1;
+        if (palindromeLength > resLen) {
+          res = s.Substring(left, palindromeLength);
+          resLen = palindromeLength;
+        }
+
+        left--;
+        right++;
+      }
     }
+
+    return res;
+  }
 }
