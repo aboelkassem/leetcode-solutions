e845362e6c1732938e7dafbdbb7f8b8a6d40b4d8
Restyled.io <commits@restyled.io>
2024-01-24 09:36:06 +0000

    Restyled by clang-format

--- grind75/week 4/3.Letter Combinations of a Phone Number.cs
+++ grind75/week 4/3.Letter Combinations of a Phone Number.cs
@@ -4,32 +4,29 @@
 // Time: O(4^n) which 4 is max length of digit number '9', "wxyz” or '7', "pqrs”
 // Space: O(4^n)
 public class Solution {
-    Dictionary<char, string> map = new()
-    {
-        {'2', "abc" }, {'3', "def"},
-        {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
-        {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-    };
+  Dictionary<char, string> map =
+      new() { { '2', "abc" }, { '3', "def" }, { '4', "ghi" },
+              { '5', "jkl" }, { '6', "mno" }, { '7', "pqrs" },
+              { '8', "tuv" }, { '9', "wxyz" } };
 
-    public IList<string> LetterCombinations(string digits) {
-        List<string> combinations = new();
+  public IList<string> LetterCombinations(string digits) {
+    List<string> combinations = new();
 
-        void Traverse(int index, string curStr) {
+    void Traverse(int index, string curStr) {
 
-            // base case, a combination found
-            if (curStr.Length == digits.Length)
-            {
-                combinations.Add(curStr);
-                return;
-            }
+      // base case, a combination found
+      if (curStr.Length == digits.Length) {
+        combinations.Add(curStr);
+        return;
+      }
 
-            foreach (var c in map[digits[index]])
-                Traverse(index + 1, curStr + c);
-        }
-
-        if (!string.IsNullOrEmpty(digits))
-            Traverse(0, "");
-
-        return combinations;
+      foreach (var c in map[digits[index]])
+        Traverse(index + 1, curStr + c);
     }
+
+    if (!string.IsNullOrEmpty(digits))
+      Traverse(0, "");
+
+    return combinations;
+  }
 }
