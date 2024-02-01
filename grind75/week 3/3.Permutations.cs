a108ee2bb3723c75d982ffa09e2dd5b11a158011
Restyled.io <commits@restyled.io>
2024-02-01 18:49:13 +0000

    Restyled by astyle

--- grind75/week 3/3.Permutations.cs
+++ grind75/week 3/3.Permutations.cs
@@ -41,14 +41,14 @@ public class Solution {
         var result = new List<IList<int>>();
         var queue = new Queue<List<int>>();
         queue.Enqueue(new List<int>());
-        
-        for(int i=0;i<nums.Length;i++)
+
+        for(int i=0; i<nums.Length; i++)
         {
             int size = queue.Count;
-            for(int j=0;j<size;j++)
+            for(int j=0; j<size; j++)
             {
                 var curr = queue.Dequeue(); // get latest list
-                for(int k=0;k<=curr.Count;k++) // add it into every single index
+                for(int k=0; k<=curr.Count; k++) // add it into every single index
                 {
                     var temp = new List<int>(curr); // take a copy
                     temp.Insert(k, nums[i]); // insert at index
@@ -56,10 +56,10 @@ public class Solution {
                 }
             }
         }
-        
+
         while(queue.Count > 0)
             result.Add(queue.Dequeue());
-        
+
         return result;
     }
 }
